4cf7e1756247fa982935191a131bfeb8564c493a
Bryan Dos Santos <bryandouglasxd@gmail.com>
2021-04-14 15:28:19 -0300

    update project
    add component FooterMain

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,30 @@
 <template>
   <div id="app">
     <main>
-      <div>
-        <b-navbar toggleable="lg" type="dark" variant="dark">
-          <b-navbar-brand @click="$router.push('/')">CDWorks</b-navbar-brand>
-
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item @click="$router.push('/dashboard')"
-                >Sou Freelancer</b-nav-item
-              >
-              <b-nav-item @click="$router.push('/new-job')"
-                >Busco Freelancer</b-nav-item
-              >
-            </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-              <b-nav-item @click="$router.push('/register')"
-                >Cadastrar-se</b-nav-item
-              >
-              <b-nav-item @click="$router.push('/login')">Entrar</b-nav-item>
-              <b-nav-item-dropdown right>
-                <template v-slot:button-content>
-                  <em>NOME_USUARIO</em>
-                </template>
-                <b-dropdown-item @click="$router.push('/profile/1')"
-                  >Perfil</b-dropdown-item
-                >
-                <b-dropdown-item @click="logout()">Sair</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
-      </div>
+      <NavMain />
     </main>
     <router-view />
+    <FooterMain />
   </div>
 </template>
+<script>
+import NavMain from "@/components/NavMain.vue";
+import FooterMain from "@/components/FooterMain.vue";
 
+export default {
+  components: {
+    NavMain,
+    FooterMain,
+  },
+};
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  min-height: 100vh;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+main {
+  min-height: calc(100vh - 100px);
+  margin-bottom: 35px;
 }
 </style>
