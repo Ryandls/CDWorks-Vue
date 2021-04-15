1eb2b81ddbf3a91979d236b82fbb4ffb6fd108e4
Bryan Dos Santos <bryandouglasxd@gmail.com>
2021-04-15 14:53:37 -0300

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -43,7 +43,64 @@
           </b-form>
         </b-card>
       </div>
-      <div class="col-12 col-sm-8 col-md-9 lista"></div>
+      <div class="col-12 col-sm-8 col-md-9 lista my-2">
+        <b-button
+          class="float-right"
+          @click="() => $router.push('./jobs/new')"
+          variant="success"
+        >
+          Novo Projeto</b-button
+        >
+
+        <h3>Projetos</h3>
+        <hr />
+        <div v-if="jobs && jobs.length > 0">
+          <b-card
+            v-for="job in jobs"
+            :key="job.id"
+            :title="job.title"
+            :sub-title="job.Hirer.name"
+            class="my-2"
+            bg-variant="light"
+          >
+            <b-card-text>{{ job.description }}</b-card-text>
+            <div class="row">
+              <div class="col-6">
+                <strong>Orçamento</strong>
+
+                R${{ job.budget }}
+              </div>
+              <div class="col-6">
+                <strong>Propostas até:</strong>
+
+                {{ job.deadline }}
+              </div>
+            </div>
+            <div class="text-right">
+              <hr />
+
+              <b-button variant="primary">Mais detalhes...</b-button>
+            </div>
+          </b-card>
+
+          <div class="actions text-center">
+            <b-button variant="primary" class="mr-2" @click="previus()"
+              >Anterior</b-button
+            >
+            <b-button variant="primary" @click="next()">Proximo</b-button>
+            <p class="text-muted">
+              <small>
+                Do projeto PRIMEIRO_REGISTRO até ÚLTIMO_REGISTRO - Total de
+                TOTAL_REGISTROS projetos.</small
+              >
+            </p>
+          </div>
+        </div>
+
+        <div v-else>
+          <span class="text-muted"> Nenhum projeto encontrado....</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
