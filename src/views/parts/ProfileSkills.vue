feb76a45f7218e7071f84bb9c6e168fbb547a8f9
Bryan Dos Santos <bryandouglasxd@gmail.com>
2021-04-16 16:40:15 -0300

    update  ProfileSkills
    1/2

--- src/views/parts/ProfileSkills.vue
+++ src/views/parts/ProfileSkills.vue
@@ -1,9 +1,44 @@
 <template>
   <div class="profile">
     <h2>Habilidades</h2>
+    <hr />
+    <b-alert variant="danger" v-if="errorMessage" show>{{
+      errorMessage
+    }}</b-alert>
+    <b-form @submit.prevent="onSubmit" @reset.prevent="onReset">
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <b-form-group
+            id="input-group-skill"
+            label="Habilidade"
+            label-for="input-skill"
+          >
+            <b-form-select
+              id="input-skill"
+              v-model="form.skill"
+              :options="skills"
+            ></b-form-select>
+          </b-form-group>
+        </div>
+      </div>
+    </b-form>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data: () => ({
+    user: {},
+    errorMessage: null,
+    form: {
+      skills: null,
+      level: null,
+    },
+    skills: [],
+  }),
+  methods: {},
+  async mounted() {
+    this.user = this.$parent.user;
+  },
+};
 </script>
 <style></style>
